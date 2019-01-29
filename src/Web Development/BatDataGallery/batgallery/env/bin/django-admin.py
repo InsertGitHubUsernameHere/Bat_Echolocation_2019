0bf15cf93977dd8d41c09b41553192fbc3615b5a
Thien Le <t_le@uncg.edu>
2019-01-29 13:22:42 -0500

    Initial Commit

---
+++ src/Web Development/BatDataGallery/batgallery/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/thienle/Desktop/Classes/Spring 2019/SeniorCapstone/Bat_Echolocation/src/Web Development/BatDataGallery/batgallery/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
