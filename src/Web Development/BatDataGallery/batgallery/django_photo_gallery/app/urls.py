332bf6989e265ca6bce1276e29fb42aa24aba6d4
RahimIqbal <46869481+RahimIqbal@users.noreply.github.com>
2019-03-21 15:42:59 -0400

    Add files via upload

--- src/Web Development/BatDataGallery/batgallery/django_photo_gallery/app/urls.py
+++ src/Web Development/BatDataGallery/batgallery/django_photo_gallery/app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from.import views
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
    path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
    path('logout/', LogoutView.as_view(template_name='upload.html'), name="logout"),
    path('signup/', views.signup, name='signup'),
-   path('resetpassword/', PasswordResetView, name="resetpassword")
+   path('resetpassword/', PasswordResetView.as_view(template_name='registration/resetpassword.html'), name="resetpassword"),
+   path('password_reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+   path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/resetpasswordconfirm.html'), name='password_reset_confirm'),
+   path('reset/done/', PasswordResetCompleteView.as_view(template_name='registration/resetpasswordcomplete.html'), name='password_reset_complete')
+
 ]
