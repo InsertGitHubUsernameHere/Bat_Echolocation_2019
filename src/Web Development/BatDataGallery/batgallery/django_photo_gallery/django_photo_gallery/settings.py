71de25693be676ba1ca97cb3b3c2b81d141b9e97
RahimIqbal <46869481+RahimIqbal@users.noreply.github.com>
2019-03-21 15:44:29 -0400

    Add files via upload
    Fixed error and create SMTP

--- src/Web Development/BatDataGallery/batgallery/django_photo_gallery/django_photo_gallery/settings.py
+++ src/Web Development/BatDataGallery/batgallery/django_photo_gallery/django_photo_gallery/settings.py
@@ -140,13 +140,18 @@ CACHES = {
     }
 }
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
+
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 )
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'aasdsa203@gmail.com'
+EMAIL_HOST_PASSWORD = 'hitman007' 
