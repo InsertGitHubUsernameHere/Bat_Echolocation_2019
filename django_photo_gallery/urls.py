fefcb501ba8c9a56f3009f8b6340717373c1f6a7
Hadi Soufi <soufihadi@yahoo.com>
2019-04-06 22:48:09 -0400

    updated url to path/re_path

--- django_photo_gallery/urls.py
+++ django_photo_gallery/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -17,16 +17,16 @@ urlpatterns = [
     path('', include('app.urls')),
     path('upload', app.views.upload, name='upload'),
     path('display', app.views.displayImages, name='displayImages'),
-    url(r'^$', app.views.gallery, name='gallery'),
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
-    url(r'^(?P<slug>[-\w]+)$', app.views.AlbumDetail.as_view(), name='album'),  # app.views.AlbumView.as_view()
+    path('$', app.views.gallery, name = 'gallery'),
+
+    re_path(r'^(?P<slug>[-\w]+)$', app.views.AlbumDetail.as_view(), name='album'),  # app.views.AlbumView.as_view()
 
     # Auth related urls
-    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', app.views.logout, {'next_page': '/', }, name='logout'),
+    path('accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/$', app.views.logout, {'next_page': '/', }, name='logout'),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
