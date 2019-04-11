b957d6027de68c310e22853aebecdabc6881384d
RahimIqbal <46869481+RahimIqbal@users.noreply.github.com>
2019-04-11 17:48:27 -0400

    Fixed error of re displaying images

--- util/db_API.py
+++ util/db_API.py
@@ -3,6 +3,7 @@ from util import CNN
 import pandas as pd
 import os
 import glob
+import numpy as np 
 import json
 from util import bat
 import sqlite3
@@ -14,10 +15,8 @@ import zipfile
 import numpy as np
 from numpy.polynomial.polynomial import polyfit
 from keras.models import load_model
+import sqlite3
 import shutil
-
-
-# 0: Source ZC name, 1: image data, 2: classified, 3: metadata, 4: uid
 
 def get_tables():
     conn = sqlite3.connect('../db.sqlite3')
@@ -27,21 +26,15 @@ def get_tables():
     return tables
 
 
+# Clean ZC file and add to DB
 def insert(uid, file_name, file):
-    """ Clean ZC file and add to DB """
     conn = sqlite3.connect('../db.sqlite3')
-    c = conn.cursor()
 
     # If images table doesn't exist yet, make it
     if 'images' not in get_tables():
-
-        query = '''CREATE TABLE images (name VARCHAR(255),
-                                        raw BLOB,
-                                        classification VARCHAR(255),
-                                        metadata VARCHAR(255),
-                                        uid INTEGER);'''
         c = conn.cursor()
-        c.execute(query)
+        with conn:
+            c.execute('CREATE TABLE images (name VARCHAR(255), raw BLOB, classification VARCHAR(255), metadata VARCHAR(255), uid INTEGER);')
 
     # Extract data from ZC file
     try:
@@ -58,20 +51,21 @@ def insert(uid, file_name, file):
     metadata_str = json.dumps(metadata)
 
     # Get cleaned pulse data
-    pulses = data_processing.clean_graph(filename='', graph=[raw[0], raw[1]])
+    pulses = data_processing.clean_graph(filename = '', graph=[raw[0], raw[1]])
 
     # Add each pulse and associated metadata to DB
     with conn:
+        c = conn.cursor()
         c.execute('SELECT * from images',)
         found = c.fetchall()
-        if not any(i[0] == file_name and i[4] == uid for i in found):
-            for pulse in pulses:
-                c.execute('INSERT INTO images VALUES (?, ?, ?, ?, ?);',
-                          (file_name, str(pulse), '0', metadata_str, uid))
+        if not any(i[0] == file_name for i in found):
+            for pulse in pulses:    
+                c.execute('INSERT INTO images VALUES (?, ?, ?, ?, ?);', (file_name, str(pulse), '0', metadata_str, uid))
 
 
+# Add zip file to DB
 def insert_zip(uid, outdir, file_name, file):
-    """Add all contents of zip file to DB"""
+    conn = sqlite3.connect('../db.sqlite3')
 
     # Extract zip and delete it
     z_name = outdir + '/' + 'temp.zip'
@@ -83,13 +77,12 @@ def insert_zip(uid, outdir, file_name, file):
     os.remove(z_name)
 
     # Select all valid files for processing
-    # TODO- Extend with more filetypes.
-    #       Make global list of accepted extensions?
+    # TODO- extend with more filetypes. make global list of accepted extensions?
     filenames = glob.glob(outdir + '/**/*#', recursive=True)
     filenames.extend(glob.glob(outdir + '/**/*.zip', recursive=True))
     filenames.extend(glob.glob(outdir + '/**/*.zca', recursive=True))
 
-    # Recursively handle zip files, insert others to database.
+    # Iterate through each file. Recursively handle zip files, insert others to database.
     # TODO- extend with more filetypes
     for file in filenames:
         f = open(file, 'rb')
@@ -101,36 +94,42 @@ def insert_zip(uid, outdir, file_name, file):
         else:
             insert(uid, os.path.basename(file), z)
 
-        # Delete file after processing
         f.close()
         os.remove(file)
 
+    # Empty directory when finished
+    files = glob.glob(outdir)
 
+
+# Load images from DB and render
+# 0: Source ZC name, 1: image data, 2: classified, 3: metadata, 4: uid
 def load_images(uid, outdir):
-    """ Load images from DB and render """
-
     conn = sqlite3.connect('../db.sqlite3')
-    c = conn.cursor()
 
     # Load users image data
-    c.execute('SELECT * FROM images WHERE uid=?', (uid,))
+    c = conn.cursor()
+    c.execute('SELECT * FROM images')
     table = c.fetchall()
-    table = [t for t in table if t[4] == uid]
+
     # Load CNN
-    __location__ = os.path.realpath(os.path.join(
-        os.getcwd(), os.path.dirname(__file__)))
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     model = load_model(__location__ + '/CNN200x300ep30.h5')
 
     fig, ax = plt.subplots()
     for i, row in enumerate(table):
+
+        # Only render current user data
+        if not row[4] == uid:
+            continue
+
         # Only render files that haven't been rendered yet
         if row[2] == '1':
             continue
 
         # Mark pulse as rendered
-        sql = '''UPDATE images
-                 SET classification = '1'
-                 WHERE ''' + str(i) + ''' = ROWID'''
+        sql = ''' UPDATE images
+                  SET classification = '1'
+                  WHERE ''' + str(i) + ''' = ROWID'''
         c.execute(sql)
 
         # Convert DB string to 2d list
@@ -141,41 +140,49 @@ def load_images(uid, outdir):
         y = [point[1] for point in pulse]
 
         # Generate image path
-        save_path = outdir + '/' + '^_' + \
-            row[0].replace('#', '') + '_' + str(i) + '.png'
+        save_path = outdir + '/' + '^_' + row[0].replace('#', '') + '_' + str(i) + '.png'
 
         # Render the image
+
         ax.axis('off')
         ax.scatter(x, y)
-        fig.savefig(save_path, transparent=True, dpi=50)
+
+        try:
+            fig.savefig(save_path, transparent=True, dpi=50)
+        except:
+            pass
+
         plt.cla()
         gc.collect()
 
         # Classify as normal or abnormal
         # TODO- perform at insert?
         if not CNN.classifyCNN(save_path, model) == 0:
-            os.rename(save_path, save_path.replace('^', 'e'))
+            try:
+                os.rename(save_path, save_path.replace('^', 'e'))
+            except:
+                os.remove(save_path.replace('^', 'e'))
+                os.rename(save_path, save_path.replace('^', 'e'))
         else:
-            os.rename(save_path, save_path.replace('^', 'a'))
-
+            try:
+                os.rename(save_path, save_path.replace('^', 'a'))
+            except:
+                os.remove(save_path.replace('^', 'a'))
+                os.rename(save_path, save_path.replace('^', 'a'))
 
 def select_images(name=None, classification=None):
     conn = sqlite3.connect('../db.sqlite3')
     c = conn.cursor()
-    # both name==... and classification==...
-    if name is not None and classification is not None:
-        c.execute('SELECT * FROM images WHERE name=? AND classification=?;',
-                  (name, classification))
+    if name is not None and classification is not None:  # both name==... and classification==...
+        c.execute('SELECT * FROM images WHERE name=? AND classification=?;', (name, classification))
     elif name is not None:  # name==...
         c.execute('SELECT * FROM images WHERE name=?;', (name,))
     elif classification is not None:  # classification==...
-        c.execute('SELECT * FROM images WHERE classification=?;',
-                  (classification,))
+        c.execute('SELECT * FROM images WHERE classification=?;', (classification,))
     else:  # both name==None and classification==None
         c.execute('SELECT * FROM images;')
 
-    df = pd.DataFrame.from_records(
-        c.fetchall(), columns=['name', 'raw', 'classification', 'metadata'])
+    df = pd.DataFrame.from_records(c.fetchall(), columns=['name', 'raw', 'classification', 'metadata'])
     print(df)  # temporary
 
     # convert binary into PNG images and store them in .../media folder
@@ -190,47 +197,31 @@ def load_metadata(uid):
 
     return [[row[0], row[3]] for row in table if row[4] == uid]
 
-
+# function to fetch user information (for back-end only)
 def get_users():
-    """Function to fetch user information (for back-end only)"""
     conn = sqlite3.connect('../db.sqlite3')
     c = conn.cursor()
-
-    query = '''SELECT username,
-                      password,
-                      first_name,
-                      last_name,
-                      organization
-               FROM auth_user;'''
-
-    cols = ['username', 'password',
-            'first_name', 'last_name', 'organization']
-
-    c.execute(query)
-    df = pd.DataFrame.from_records(c.fetchall(), columns=cols)
+    c.execute('SELECT username, password, first_name, last_name, organization FROM auth_user;')
+    df = pd.DataFrame.from_records(c.fetchall(), columns=['username', 'password', 'first_name', 'last_name', 'organization'])
     print(df)
 
-
-def erase_data(uid):
-    """ Remove all user data from images table. Only call on logout """
+# Remove all user data from images table. Call on 'logout'
+def erase_data(uid) :
     conn = sqlite3.connect('../db.sqlite3')
     c = conn.cursor()
     c.execute('DELETE FROM images WHERE uid=?', (uid,))
 
-
-def make_zip(indir, outdir):
-    """ Make a zip file with all generated pulses """
-
+def make_zip(indir, outdir) :
     # Make appropriate directories if possible
     noutdir = outdir + '/buildazip'
     try:
         os.makedirs(noutdir)
-    except Exception e:
+    except:
         pass
     try:
         os.mkdir(noutdir + '/abnormal')
         os.mkdir(noutdir + '/echolocation')
-    except Exception e:
+    except:
         pass
 
     # Get all files in directory
@@ -240,9 +231,9 @@ def make_zip(indir, outdir):
     for file in onlyfiles:
         f = file
         file = indir + '/' + file
-        if f.startswith('a_'):
+        if f.startswith('a_') :
             os.rename(file, os.path.join(noutdir, 'abnormal', f))
-        elif f.startswith('e_'):
+        elif f.startswith('e_') :
             os.rename(file, os.path.join(noutdir, 'echolocation', f))
 
     # Make zip and load into memory
@@ -250,9 +241,9 @@ def make_zip(indir, outdir):
     z = open(outdir + '/results.zip', 'rb')
     m = z.read()
     z.close()
-
+    
     # Delete everything
     shutil.rmtree(noutdir)
     shutil.rmtree(indir)
-
+    
     return 'results.zip', m
