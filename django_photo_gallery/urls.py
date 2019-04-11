7ea27019545515b0e6432df308027113b363e9a5
RahimIqbal <46869481+RahimIqbal@users.noreply.github.com>
2019-04-11 17:47:12 -0400

    Add graph url

--- django_photo_gallery/urls.py
+++ django_photo_gallery/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/resetpasswordconfirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/resetpasswordcomplete.html'), name='password_reset_complete'),
 
+    path('graph', app.views.draw_graph, name='graph'),
     path('upload', app.views.upload, name='upload'),
     path('download', app.views.download_zip),
     path('display', app.views.displayImages, name='displayImages'),
@@ -28,11 +29,6 @@ urlpatterns = [
     re_path(r'^(?P<slug>[-\w]+)$', app.views.AlbumDetail.as_view(), name='album'),
 
     # Auth related urls
-    # old
-    #path('accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    #path('accounts/logout/$', app.views.logout, {'next_page': '/',}, name='logout'),
-
-    # new
     re_path(r'accounts/login/$', auth_views.LoginView.as_view(), name='login'),
     re_path(r'accounts/logout/$', app.views.logout, {'next_page': '/',}, name='logout'),
 
