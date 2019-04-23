5ff5331f0f38ebf7a1831ed04c53accf8d9980c4
HadiSoufi <soufihadi@yahoo.com>
2019-04-22 21:05:39 -0400

    more work on async render

--- batalog/tasks.py
+++ batalog/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+from celery import task
 from util import db_API
 
-@shared_task(name="render_images")
+@task
 def render_images(uid, outdir):
     db_API.render_images(uid, outdir)
